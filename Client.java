623cf7abd45b8ed96a1f89c81d9dfe91debe5582
Sarawalee Tangjittham <tangjittham_s@silpakorn.edu>
2022-11-28 20:12:57 +0700

    initial commit

---
+++ Client.java
@@ -0,0 +1,164 @@
+import java.net.*;
+import java.nio.channels.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class Client {
+    private Socket socket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private SocketChannel socketChannel;
+    private final String foldercopy = "D:/OS/Client/Copy/";  //<<<<<<<<<<<<<<<<<<<<<<<<
+    private final String folderzero = "D:/OS/Client/Zero/";  //<<<<<<<<<<<<<<<<<<<<<<<<
+    String[] filename = new String[10000];
+    
+    public final void connection() {
+        try {
+            socket = new Socket("192.168.1.20", 5000);  //<<<<<<<<<<<<<<<<<<<<<<<<
+            dis = new DataInputStream(socket.getInputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
+            socketChannel = SocketChannel.open(new InetSocketAddress("192.168.1.20",5001)); //<<<<<<<<<<<<<<<<<<<<<<<<
+        } catch (IOException e) {}
+    }
+
+  
+    
+    public final void showFile() {
+        
+        System.out.println(" __________________________________________");
+        System.out.println("|                                          |");
+        System.out.println("|             File in Server               |");
+        System.out.println("|__________________________________________|");
+        System.out.println("|                                          |");
+        try{
+             int length = dis.readInt();
+            for (int i = 0; i < length; i++) {
+                filename[i]=dis.readUTF();
+                System.out.println("     File: " + filename[i]);
+            } 
+        }catch (IOException e) { }
+        
+        
+        System.out.println("|                                          |");
+        System.out.println("|__________________________________________|");
+    }
+
+    public final void askUser() {
+        String namefile = "";
+        boolean check;
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            showFile();
+            System.out.print("[ CLEINT ] Select File: ");
+            namefile = sc.next();
+            if(namefile.equalsIgnoreCase("EXIT")){
+                break;
+            }
+            try {
+                dos.writeUTF(namefile);
+                check =dis.readBoolean();
+                if (check==false) {
+                    System.out.println("Not Found \n"); 
+                    break;
+                }
+                System.out.print("1.Copy\n2.Zero Copy\nSelect type to copy : ");
+                String type = sc.next();
+                if (!type.equals("1") && !type.equals("2")) {
+                    namefile="";
+                    check=false;
+                    System.out.println("์Not Select type to copy");
+                    break;
+                }
+                
+                
+                dos.writeUTF(type);
+                long size = dis.readLong();
+                String filePathcopy = foldercopy + namefile;
+                String filePathzero = folderzero + namefile;
+                if(type.equals("1"))
+                    copy(filePathcopy, size);
+                else
+                    zeroCopy(filePathzero, size);
+                break;
+            } 
+            catch (IOException ex) {}
+        }
+    }
+    
+    public void copy(String filePath, long size) {
+        FileOutputStream fos = null;
+        try {
+            long start = System.currentTimeMillis();
+                
+                
+            fos = new FileOutputStream(filePath);
+            byte[] buffer = new byte[8*1024];
+            int read;
+            long currentRead = 0;
+            while (currentRead < size && (read = dis.read(buffer)) != -1) {
+                fos.write(buffer, 0, read);
+                currentRead += read;
+            }
+            System.out.println("Copy Success");
+            long end = System.currentTimeMillis();
+            long time = end-start;
+            System.out.println("Time : "+time+" ms\n");
+            dos.writeLong(time);
+        } catch (IOException e) { }
+        finally {
+            try {
+                if (fos != null) 
+                    fos.close();
+            } catch (IOException e) {
+                disconnect();
+            }
+        }
+    }
+    
+    public final void zeroCopy(String filePath, long size){
+        FileChannel destination = null;
+        try{
+            long start = System.currentTimeMillis();
+            destination = new FileOutputStream(filePath).getChannel();
+            long currentRead = 0;
+            long read;
+            while(currentRead < size && (read = destination.transferFrom(socketChannel, currentRead, size - currentRead)) != -1)
+                currentRead += read;
+            System.out.println("Zero Copy Success");
+            long end = System.currentTimeMillis();
+            long time = end-start;
+            System.out.println("Time : "+time+" ms\n");
+            dos.writeLong(time);
+        } catch (IOException e){}
+        finally{
+            try{
+                if(destination != null)
+                    destination.close();
+            } catch (IOException e){
+                disconnect();
+            }
+        }
+    }
+    
+    public void disconnect(){
+        try{
+            if(dis != null)
+                dis.close();
+            if(dos != null)
+                dos.close();
+            if(socket != null)
+                socket.close();
+            if(socketChannel != null)
+                socketChannel.close();
+        } catch (IOException e){ }
+    }
+    
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connection();
+        client.askUser();
+    }
+
+
+}
---
+++ Server.java
@@ -0,0 +1,176 @@
+import java.io.*;
+import java.nio.channels.*;
+//import java.nio.file.Files;
+//import java.util.jar.Attributes.Name;
+import java.net.*;
+
+/**
+ * server
+ */
+public class Server {
+    public static int numclient;
+    private ServerSocket serverSocket;
+    private ServerSocketChannel serverChannel;
+    static File folder = new File("D:/OS/Server/");   //<<<<<<<<<<<<<<<<<<<<<<<<
+    private final File[] listOfFiles = folder.listFiles();
+    String[] filename = new String[10000];
+    
+    
+    public final void connection(){
+        try {
+            serverSocket = new ServerSocket(5000);  //<<<<<<<<<<<<<<<<<<<<<<<<
+            serverChannel = ServerSocketChannel.open();
+            serverChannel.bind(new InetSocketAddress(5001)); //<<<<<<<<<<<<<<<<<<<<<<<<
+            
+            System.out.println("[ Wait for Client join the Server ]");
+            while (true) {
+                Socket socketClient = serverSocket.accept();
+                DataInputStream dis = new DataInputStream(socketClient.getInputStream());
+                DataOutputStream dos = new DataOutputStream(socketClient.getOutputStream());
+                SocketChannel socketChannel = serverChannel.accept();
+                System.out.println("============================================");
+                System.out.println("|           New Client Connected           |");
+                System.out.println("|               Client No: " + (numclient+1)+ "               |");
+                System.out.println("============================================");
+                new Thread(new ClientHandle(socketClient, dis,  dos, socketChannel, ++numclient)).start();
+            }
+        } catch (IOException e) {}
+    }
+
+    class ClientHandle implements Runnable {
+
+        private final int clientNo;
+        private final Socket socket;
+        private final DataInputStream dis;
+        private final DataOutputStream dos;
+        private final SocketChannel socketChannel;
+
+        public ClientHandle(Socket socket, DataInputStream dis, DataOutputStream dos, SocketChannel socketChannel, int clientNo) {
+            this.socket = socket;
+            this.dis = dis;
+            this.dos = dos;
+            this.socketChannel = socketChannel;
+            this.clientNo = clientNo;
+            
+        }
+        void sendList(){
+            try{
+                dos.writeInt(listOfFiles.length);
+                for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    filename[i] = listOfFiles[i].getName();
+                    dos.writeUTF(filename[i] );
+                    dos.flush();;
+                }
+            } 
+            }catch (IOException e) { }
+            
+        }
+
+        @Override
+        public void run() {
+            sendList();  
+            try {
+                boolean check = false;
+                while (true) {
+                    String namefile = dis.readUTF();
+                    String filePath = "D:/OS/Server/"+namefile; //<<<<<<<<<<<<<<<<<<<<<<<<
+                    File file = new File(filePath);
+                    for (int i = 0; i < listOfFiles.length; ++i) {
+                        if (namefile.equalsIgnoreCase(filename[i])) {
+                            System.out.println("[ SERVER ] Send File List ");
+                            check = true;      
+                        }     
+                    }
+                    dos.writeBoolean(check);
+                    dos.flush();
+
+                    String type = dis.readUTF();
+                    long size = file.length();
+                    dos.writeLong(size);
+                    System.out.print("[ Client " + clientNo + " ] Select File name: "+ namefile);
+                    
+                    if(type.equals("1")){
+                        System.out.println(" : copy file");
+                        copy(filePath, size,namefile);
+                    }
+                    else if(type.equals("2")){
+                        System.out.println(" : zero copy file");
+                        zeroCopy(filePath, size,namefile);
+                    }
+                    long time=dis.readLong();
+                    System.out.println("Time : "+time+" ms\n");
+                }
+            } catch (IOException ex) {
+                disconnect();
+            }
+        }
+ 
+        public void copy(String filePath, long size,String namefile) {
+            FileInputStream readfile = null;
+            try {
+                readfile = new FileInputStream(filePath);
+                byte[] buffer = new byte[8*1024];
+                int read;
+                long currentRead = 0;
+                while (currentRead < size && (read = readfile.read(buffer)) != -1) {
+                    dos.write(buffer, 0, read);
+                    currentRead += read;
+                }
+                System.out.println("[ SERVER ] Send File:  " + namefile +" : copy file "+ " Successful!!");
+            } 
+            catch (IOException e) {} 
+            finally {
+                try {
+                    if (readfile != null)
+                        readfile.close();
+                        
+                } catch (IOException e) {
+                    disconnect();
+                }
+            }
+        }
+        
+        public void zeroCopy(String filePath, long size,String namefile){
+            FileChannel source = null;
+            try{
+                source = new FileInputStream(filePath).getChannel();
+                long currentRead = 0;
+                long read;
+                while(currentRead < size && (read = source.transferTo(currentRead, size - currentRead, socketChannel)) != -1){
+                    currentRead += read;
+                }               
+                System.out.println("[ SERVER ] Send File:  " + namefile +" : zero copy "+ " Successful!!");
+            }
+            catch (IOException e){}
+            finally{
+                try{
+                    if(source != null)
+                        source.close();
+                        
+                } catch (IOException e){
+                    disconnect();
+                }
+            }
+        }
+        
+        public void disconnect(){
+            System.out.println("[ SERVER ] Client: " + clientNo + " Disconnected!\n");
+            try{
+                if(dis != null)
+                    dis.close();
+                if(dos != null)
+                    dos.close();
+                if(socket != null)
+                    socket.close();
+                if(socketChannel != null)
+                    socketChannel.close();
+            } catch (IOException e){ }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+       Server server = new Server();
+       server.connection();
+    }  
+}
